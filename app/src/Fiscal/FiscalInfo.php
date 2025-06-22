d9793fe3e1d6ccfb8146df52f68250a0c3804a5d
roxblnfk <roxblnfk@ya.ru>
2025-06-22 19:03:04 +0400

    Refactor FiscalInfo and FiscalService classes to implement constructor and restore activity method

--- app/src/Fiscal/FiscalInfo.php
+++ app/src/Fiscal/FiscalInfo.php
@@ -4,26 +4,12 @@ declare(strict_types=1);
 
 namespace App\Fiscal;
 
-use Temporal\Activity\ActivityMethod;
-use Temporal\Exception\Failure\ApplicationFailure;
+use Ramsey\Uuid\UuidInterface;
 
-class FiscalInfo implements FiscalActivity
+class FiscalInfo
 {
-    #[ActivityMethod]
-    public function fart(FiscalInfo $info): void
-    {
-        // Simulate some processing logic that may fail
-        \mt_rand(0, 100) > 10 or throw new ApplicationFailure(
-            message: 'Fatal error occurred',
-            type: 'FatalError',
-            nonRetryable: true,
-        );
-
-        // Simulate a service unavailability scenario
-        \mt_rand(0, 100) > 50 or throw new ApplicationFailure(
-            message: 'Service is unavailable',
-            type: 'ServiceUnavailable',
-            nonRetryable: true,
-        );
-    }
+    public function __construct(
+        public UuidInterface $uuid,
+        public int $amount,
+    ) {}
 }
--- app/src/Fiscal/Internal/FiscalService.php
+++ app/src/Fiscal/Internal/FiscalService.php
@@ -2,4 +2,28 @@
 
 namespace App\Fiscal\Internal;
 
-class FiscalService {}
+use App\Fiscal\FiscalActivity;
+use App\Fiscal\FiscalInfo;
+use Temporal\Activity\ActivityMethod;
+use Temporal\Exception\Failure\ApplicationFailure;
+
+class FiscalService implements FiscalActivity
+{
+    #[ActivityMethod]
+    public function fart(FiscalInfo $info): void
+    {
+        // Simulate some processing logic that may fail
+        \mt_rand(0, 100) > 10 or throw new ApplicationFailure(
+            message: 'Fatal error occurred',
+            type: 'FatalError',
+            nonRetryable: true,
+        );
+
+        // Simulate a service unavailability scenario
+        \mt_rand(0, 100) > 50 or throw new ApplicationFailure(
+            message: 'Service is unavailable',
+            type: 'ServiceUnavailable',
+            nonRetryable: true,
+        );
+    }
+}
